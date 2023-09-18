234620b4cc4813d13f4db7ea755606bead6c5d80
Devon Bear <itsdevbear@berachain.com>
2023-09-18 13:02:53 -0400

    annoying but yolo

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -20,67 +20,69 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 )
 
 // StateDB is an EVM database for full state querying.
 type StateDB interface {
-	CreateAccount(common.Address)
+	state.StateDBI
+	// TODO: streamline this interface
+	// CreateAccount(common.Address)
 
-	SubBalance(common.Address, *big.Int)
-	AddBalance(common.Address, *big.Int)
-	GetBalance(common.Address) *big.Int
+	// SubBalance(common.Address, *big.Int)
+	// AddBalance(common.Address, *big.Int)
+	// GetBalance(common.Address) *big.Int
 
-	GetNonce(common.Address) uint64
-	SetNonce(common.Address, uint64)
+	// GetNonce(common.Address) uint64
+	// SetNonce(common.Address, uint64)
 
-	GetCodeHash(common.Address) common.Hash
-	GetCode(common.Address) []byte
-	SetCode(common.Address, []byte)
-	GetCodeSize(common.Address) int
+	// GetCodeHash(common.Address) common.Hash
+	// GetCode(common.Address) []byte
+	// SetCode(common.Address, []byte)
+	// GetCodeSize(common.Address) int
 
-	AddRefund(uint64)
-	SubRefund(uint64)
-	GetRefund() uint64
+	// AddRefund(uint64)
+	// SubRefund(uint64)
+	// GetRefund() uint64
 
-	GetCommittedState(common.Address, common.Hash) common.Hash
-	GetState(common.Address, common.Hash) common.Hash
-	SetState(common.Address, common.Hash, common.Hash)
+	// GetCommittedState(common.Address, common.Hash) common.Hash
+	// GetState(common.Address, common.Hash) common.Hash
+	// SetState(common.Address, common.Hash, common.Hash)
 
-	GetTransientState(addr common.Address, key common.Hash) common.Hash
-	SetTransientState(addr common.Address, key, value common.Hash)
+	// GetTransientState(addr common.Address, key common.Hash) common.Hash
+	// SetTransientState(addr common.Address, key, value common.Hash)
 
-	SelfDestruct(common.Address)
-	HasSelfDestructed(common.Address) bool
+	// SelfDestruct(common.Address)
+	// HasSelfDestructed(common.Address) bool
 
-	Selfdestruct6780(common.Address)
+	// Selfdestruct6780(common.Address)
 
-	// Exist reports whether the given account exists in state.
-	// Notably this should also return true for self-destructed accounts.
-	Exist(common.Address) bool
-	// Empty returns whether the given account is empty. Empty
-	// is defined according to EIP161 (balance = nonce = code = 0).
-	Empty(common.Address) bool
+	// // Exist reports whether the given account exists in state.
+	// // Notably this should also return true for self-destructed accounts.
+	// Exist(common.Address) bool
+	// // Empty returns whether the given account is empty. Empty
+	// // is defined according to EIP161 (balance = nonce = code = 0).
+	// Empty(common.Address) bool
 
-	AddressInAccessList(addr common.Address) bool
-	SlotInAccessList(addr common.Address, slot common.Hash) (addressOk bool, slotOk bool)
-	// AddAddressToAccessList adds the given address to the access list. This operation is safe to perform
-	// even if the feature/fork is not active yet
-	AddAddressToAccessList(addr common.Address)
-	// AddSlotToAccessList adds the given (address,slot) to the access list. This operation is safe to perform
-	// even if the feature/fork is not active yet
-	AddSlotToAccessList(addr common.Address, slot common.Hash)
-	Prepare(rules params.Rules, sender, coinbase common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
+	// AddressInAccessList(addr common.Address) bool
+	// SlotInAccessList(addr common.Address, slot common.Hash) (addressOk bool, slotOk bool)
+	// // AddAddressToAccessList adds the given address to the access list. This operation is safe to perform
+	// // even if the feature/fork is not active yet
+	// AddAddressToAccessList(addr common.Address)
+	// // AddSlotToAccessList adds the given (address,slot) to the access list. This operation is safe to perform
+	// // even if the feature/fork is not active yet
+	// AddSlotToAccessList(addr common.Address, slot common.Hash)
+	// Prepare(rules params.Rules, sender, coinbase common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
 
-	RevertToSnapshot(int)
-	Snapshot() int
+	// RevertToSnapshot(int)
+	// Snapshot() int
 
-	AddLog(*types.Log)
-	AddPreimage(common.Hash, []byte)
+	// AddLog(*types.Log)
+	// AddPreimage(common.Hash, []byte)
 
-	Error() error
+	// Error() error
 }
 
 // CallContext provides a basic interface for the EVM calling conventions. The EVM
