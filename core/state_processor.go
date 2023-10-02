0c73bc8663ae6db19ac529c304fdfe932f645242
Devon Bear <itsdevbear@berachain.com>
2023-10-02 18:27:28 -0400

    spblockchain

--- core/state_processor.go
+++ core/state_processor.go
@@ -32,18 +32,23 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 )
 
+type SPBlockchain interface {
+	ChainContext
+	consensus.ChainHeaderReader
+}
+
 // StateProcessor is a basic Processor, which takes care of transitioning
 // state from one point to another.
 //
 // StateProcessor implements Processor.
 type StateProcessor struct {
 	config *params.ChainConfig // Chain configuration options
-	bc     *BlockChain         // Canonical block chain
+	bc     SPBlockchain        // Canonical block chain
 	engine consensus.Engine    // Consensus engine used for block rewards
 }
 
 // NewStateProcessor initialises a new StateProcessor.
-func NewStateProcessor(config *params.ChainConfig, bc *BlockChain, engine consensus.Engine) *StateProcessor {
+func NewStateProcessor(config *params.ChainConfig, bc SPBlockchain, engine consensus.Engine) *StateProcessor {
 	return &StateProcessor{
 		config: config,
 		bc:     bc,
