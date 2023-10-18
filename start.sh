fe6dbacddc73a773803511bc9faecd6c9ac7d32e
Devon Bear <itsdevbear@berachain.com>
2023-10-18 17:17:52 -0400

    ree

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ rm -rf data
 mkdir data
 mkdir data/node1
 ./build/bin/geth --datadir data/node1/ init genesis.json
-./build/bin/geth --datadir data/node1 --ipcdisable --port 30301 --networkid 7 --http --http.addr 'localhost' --http.port 8545 --http.api 'admin,debug,eth,miner,net,personal,txpool,web3' --allow-insecure-unlock
+./build/bin/geth --datadir data/node1 --ipcdisable --port 30301 --networkid 7 --http --http.addr 'localhost' --http.port 8545 --http.api 'admin,engine,debug,eth,miner,net,personal,txpool,web3' --allow-insecure-unlock --syncmode full
